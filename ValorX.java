e0cf4d2549a0a67c155d757bf3789bbcbbd83fb8
LuanBorges19 <144127748+LuanBg@users.noreply.github.com>
2024-04-16 21:23:37 -0300

    ValorX.java

---
+++ ValorX.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String []angs){
+        String nome="Luan Borges";
+        float x= -2;
+        float a= 4+ 3* x + 5* x - 10- x;
+        float b= 5*x - 4*x + 10*x - 10*x;
+        float c= 10*x - 20*x + 10/x + 5*x -3*x;
+        System.out.println("O resultado é: "+ a);
+        System.out.println("O resultado é: "+ b);
+        System.out.println("O resultado é: "+ c);
+    }
+}
